22d5af4cdf2c986aba864e01f775d49c8e931f37
lazycatL <510754059@qq.com>
2017-08-20 17:37:25 +0800

    初始化

---
+++ login.vue
@@ -0,0 +1,92 @@
+
+<template>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h1 class="title">生产管理系统-登录</h1>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="isLogin" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      logining: false,
+      ruleForm2: {
+        account: 'admin',
+        checkPass: '123456'
+      },
+      rules2: {
+        account: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+        ],
+        checkPass: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+        ]
+      },
+      checked: true
+    };
+  },
+  methods: {
+      ﻿isLogin:function() {
+          this.$refs.ruleForm2.validate((valid) => {
+          if (valid) {
+            var loginParams = new URLSearchParams();
+            loginParams.append("type", 'vue');
+            console.log(this.ruleForm2.account);
+            loginParams.append("username", (this.ruleForm2.account));
+            loginParams.append("password", (this.ruleForm2.checkPass));
+
+            this.func.ajaxPost(this.api.userLogin,loginParams, res => {
+                if (res.data.result === "ok") {
+                  this.$message({message:'登陆成功',type:'success'});
+                  this.logining=true;
+
+                  sessionStorage.setItem('user', JSON.stringify(res.data.user));
+                  this.$router.push('/index');
+              }else{
+                this.$message({message:'用户名或密码错误!',type:'error'});
+            }
+
+          });
+
+          }
+        });
+    }
+  }
+}
+</script>
+<style scoped>
+    .login-container{
+         box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+        -moz-border-radius: 5px;
+        background-clip: padding-box;
+        margin: 80px auto;
+        width: 350px;
+        padding: 35px 35px 15px 35px;
+        background: #fff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+    .title {
+      margin: 0px auto 20px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 0px 0px;
+    }
+</style>
