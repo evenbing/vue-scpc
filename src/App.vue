a0963d4e9c7fbfa54c527c954d03e3d6691ddd9c
lazycatL <510754059@qq.com>
2017-08-20 21:38:43 +0800

    备份提交

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,12 @@
         <router-view></router-view>
     </div>
 </template>
-<<<<<<< HEAD
 
 <script>
     export default {
       name: 'app',
       components: {
       },
-=======
-<script>
-    export default {
->>>>>>> f674a1a478c56054f50fae2602b4db688ad111f4
         data() {
             return {
 
@@ -29,8 +24,4 @@
 
         }
     };
-<<<<<<< HEAD
 </script>
-=======
-</script>
->>>>>>> f674a1a478c56054f50fae2602b4db688ad111f4
