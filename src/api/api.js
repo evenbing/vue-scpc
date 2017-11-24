b5971220f4ab1dbbf5ddaa7c1fda371527398483
lazycatL <510754059@qq.com>
2017-11-24 15:16:45 +0800

    增加树状表格

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,29 @@
 'use strict';
 
 //let host = '/api';
-//let host ='http://192.168.1.254/scpc';
-let host ='http://192.168.0.118/scpc';
+// let host ='http://192.168.1.254/scpc';
+let host ='http://127.0.0.1:8089/scpc';
+
 module.exports =  {
     //user
     userLogin: host + '/login.action',
     getMenu:host+'/common_loadMenuTree.action',
     //res
     getResConfig:host + '/resmgr_getTableColumns.action',
-    getResList:host + '/resmgr_queryTableData.action',
-    exportData:host+'/resmgr_exportResourceData.action',
+    getTreeTableData:host+'/resmgr_getTreeTableData.action',
 
+    getResList:host + '/resmgr_queryTableData.action',
+
+    //导出
+    exportData:host+'/resmgr_exportResourceData.action',
+    exportBLData:host+'/bomInfo_exportBLData.action',
+
+    //备料情况修改
+    updateBlqk:host+'/bomInfo_updateBlzk.action',
     //统计信息
-    getTjData:host+'/ddInfo_getVueTjData.action'
-}
+    getTjData:host+'/ddInfo_getVueTjData.action',
+
+    // 加工人员加工
+    getJgryJgData:host+'/pcgl_getBomGygcJgVue.action'
+
+};
