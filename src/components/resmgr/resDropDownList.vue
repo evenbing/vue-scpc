88d54ec4db45de8142d11bced168016e6de1eccc
lazycatL <510754059@qq.com>
2017-12-25 13:46:03 +0800

    封装的下拉列表

--- src/components/resmgr/resDropDownList.vue
+++ src/components/resmgr/resDropDownList.vue
@@ -1,15 +1,44 @@
 <template>
-  <el-select placeholder="请选择">
+  <el-select @change="selectValue" filterable placeholder="请选择">
     <el-option  v-for="item in options" :key="item.id" :label="item.name" :value="item.id">
     </el-option>
   </el-select>
 </template>
 <script>
   export default {
-    props:{
-      id,
-      label,
-      sql
+    name: 'dropDownList',
+    props: {
+      tableId: {
+        type: String
+      },
+      column: {
+        type: String
+      }
+    },
+    data(){
+      return {
+        options: []
+      }
+    },
+    methods:{
+      getListData(){
+        var params = new URLSearchParams();
+        params.append("columnName",  encodeURIComponent(this.column));
+        params.append("tableId", encodeURIComponent(this.tableId));
+
+        this.func.ajaxPost(this.api.getComboboxData,params, res => {
+
+          if (res.data.length > 0) {
+              this.options = res.data
+            }
+        })
+      },
+      selectValue(item){
+        this.$emit('selectItem', {key: this.column,value: item})
+      }
+    },
+    mounted(){
+      this.getListData()
     }
   }
 </script>
